7efba34fed754c5a7b0e164c241a81bd8d3577de
theinfinityglitch <theinfinityglitch@gmail.com>
2026-07-12 15:54:58 -0300

    Add boot splash

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -2,4 +2,15 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 0;
+
+  boot.plymouth.enable = true;
+  boot.consoleLogLevel = 3;
+  boot.initrd.verbose = false;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "udev.log_level=3"
+    "systemd.show_status=auto"
+  ];
 }
